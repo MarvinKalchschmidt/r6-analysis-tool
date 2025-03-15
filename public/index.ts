8614e2fb3f748e6be30629f84d1a0f573b74efd9
Marvin <mk306@hdm-stuttgart.de>
2025-03-16 00:29:28 +0100

    add: constants.ts with images

---
+++ public/index.ts
@@ -0,0 +1,27 @@
+/* Tournament Logos*/
+import SI from "./Six_Invitational.png";
+
+/* Team Logos*/
+import FAZE from "./FaZe_Clan.png";
+import FURIA from "./FURIA.png";
+import OXG from "./Oxygen_Esports.png";
+import RCAC from "./RazaH_Company_Academy.png";
+import SSG from "./Spacestation_Gaming.png";
+import BDS from "./Team_BDS.png";
+import FLCN from "./Team_Falcons.png";
+import NWNTD from "./Unwanted.png";
+
+export {
+  /* Tournament Logos*/
+  SI,
+
+  /* Team Logos*/
+  FAZE,
+  FURIA,
+  OXG,
+  RCAC,
+  SSG,
+  BDS,
+  FLCN,
+  NWNTD,
+};
--- src/components/Scores.tsx
+++ src/components/Scores.tsx
@@ -1,28 +1,5 @@
 import Link from "next/link";
-
-type Score = {
-  logo_team_1: string;
-  symbol_team_1: string;
-  score_team_1: string;
-  logo_team_2: string;
-  symbol_team_2: string;
-  score_team_2: string;
-  team2: string;
-  date: string;
-};
-
-const SCORES: Score[] = [
-  {
-    logo_team_1: "Team Liquid",
-    symbol_team_1: "TL",
-    score_team_1: "5",
-    logo_team_2: "Team Liquid",
-    symbol_team_2: "Team Liquid",
-    score_team_2: "Team Liquid",
-    team2: "Team Liquid",
-    date: "",
-  },
-];
+import { SCORES } from "@/constants";
 
 export default function Scores() {
   return (
@@ -30,7 +7,7 @@ export default function Scores() {
       <ul className="flex justify-between items-center h-full text-xl font-bold list-none uppercase">
         {SCORES.map((score) => (
           <li className="flex items-center h-full px-8 hover:bg-(--secondary)">
-            <Link href={`/${score.logo_team_1}`}>{score.logo_team_1}</Link>
+            <Link href={`/${score.logo_team_1}`}></Link>
           </li>
         ))}
       </ul>
