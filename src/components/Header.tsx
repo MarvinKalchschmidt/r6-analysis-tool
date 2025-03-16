b4676f4928f33974d5cd256d9909f20afe1effae
Marvin <mk306@hdm-stuttgart.de>
2025-03-16 15:06:37 +0100

    feat: add highlighting to nav bar based on current page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,14 @@ import Link from "next/link";
 import Image from "next/image";
 import Logo from "../../public/logo.svg";
 import { NAV_LINKS } from "@/constants";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header
-      className={`flex items-center justify-between bg-(--tertiary) px-4 lg:px-16 gap-24 2xl:gap-48 "
+      className={`flex items-center justify-between bg-(--tertiary) px-4 lg:px-16 gap-24 2xl:gap-48"
       }`}
     >
       <Link href="/" className="flex items-center justify-between gap-4">
@@ -18,14 +21,23 @@ export default function Header() {
         </h1>
       </Link>
 
-      <nav className="hidden grow lg:flex justify-between items-center h-full">
-        <ul className="flex justify-between items-center h-full text-xl font-bold list-none uppercase">
+      <nav
+        className="hidden lg:flex justify-between items-center h-full grow"
+        aria-label="Main Navigation"
+      >
+        <ul className="flex h-full text-xl font-bold list-none uppercase">
           {NAV_LINKS.map((nav) => (
-            <li
-              key={nav.id}
-              className="flex items-center h-full px-8 hover:bg-(--secondary)"
-            >
-              <Link href={`/${nav.id}`}>{nav.title}</Link>
+            <li key={nav.id}>
+              <Link
+                href={`/${nav.id}`}
+                className={`flex items-center h-full px-8 hover:bg-(--secondary) hover:border-b-3 hover:border-(--orange) ${
+                  pathname === `/${nav.id}`
+                    ? "bg-(--secondary) border-b-3 border-blue-500"
+                    : ""
+                }`}
+              >
+                {nav.title}
+              </Link>
             </li>
           ))}
         </ul>
